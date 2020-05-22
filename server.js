727903f6bf42423f494930b5fb13a608bd4417c6
Rostyslav Broslavskyi <broslavsky.rostislaw@gmail.com>
2020-05-22 16:32:18 +0300

    update test

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 
 const port = process.env.PORT || 3001;
 
-app.use(express.static(__dirname + '/build/static'));
+app.use(express.static(__dirname + '/build'));
 
 app.get('/*', (req, res) =>
-	res.sendFile(path.join(__dirname + '/build/static/index.html'))
+	res.sendFile(path.join(__dirname + '/build/index.html'))
 );
 
 const server = http.createServer(app);
